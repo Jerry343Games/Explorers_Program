118988d966a7677701148577644a384bce45c922
Slime862 <2058456435@qq.com>
2024-04-12 23:17:56 +0800

    怪物生成（待改进

--- Explorers/Assets/sRSTz/EnemyAITest/EnemyAI.cs
+++ Explorers/Assets/sRSTz/EnemyAITest/EnemyAI.cs
@@ -18,7 +18,7 @@ public class EnemyAI : MonoBehaviour
     private AIData aiData;
 
     [SerializeField]
-    private float detectionDelay = 0.05f, aiUpdateDelay = 0.06f;//, attackDelay = 1f;
+    private float detectionDelay = 0.1f, aiUpdateDelay = 0.1f;//, attackDelay = 1f;
 
     [SerializeField]
     private float attackDistance = 0.5f;
