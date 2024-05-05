bf060e3aa4ba23a1bcd0035c243f8f20d776c3ea
Jerry <75481387+Jerry343Games@users.noreply.github.com>
2024-05-05 16:20:23 +0800

    矿机调整，新溶解Shader，开始界面视效优化

--- Explorers/Assets/_Scripts/Resources/Excavator.cs
+++ Explorers/Assets/_Scripts/Resources/Excavator.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Excavator : MonoBehaviour
 {
@@ -12,24 +14,35 @@ public class Excavator : MonoBehaviour
 
     private Resource _res;
 
+    public Material myMat;
+    private float _cutoffHeight;
+    
+
     public void Init(Resource res,float diggingDuration)
     {
+        _cutoffHeight = myMat.GetFloat("_CutoffHeight");
+        _cutoffHeight = -1;
         _res = res;
 
         _diggingTime = diggingDuration;
 
         _diggingTimer = _diggingTime;
+        
+        DOTween.To(() =>_cutoffHeight, x => _cutoffHeight=x, 1f, 1.5f);
+    
     }
 
     void Update()
     {
+        myMat.SetFloat("_CutoffHeight",_cutoffHeight);
+        
         if(_diggingTimer>0)
         {
             _diggingTimer -= Time.deltaTime;
         }
         else
         {
-            Destroy(gameObject);
+            //Destroy(gameObject);
         }
     }
 }
