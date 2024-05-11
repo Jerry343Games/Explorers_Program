7e4e0da5874deb3388eeb45722fb150020fcf5ad
7Chords <126320606+7Chords@users.noreply.github.com>
2024-05-11 21:07:56 +0800

    1

--- Explorers/Assets/_Scripts/Boss/TestInsBoss.cs
+++ Explorers/Assets/_Scripts/Boss/TestInsBoss.cs
@@ -25,32 +25,42 @@ public class TestInsBoss : MonoBehaviour
     {
         if((other.gameObject.tag == "Player" || other.gameObject.tag == "Battery") && SceneManager.Instance.isSecondLevel)
         {
-            Time.timeScale = 0;
-            Sequence s = DOTween.Sequence();
-            s.SetUpdate(UpdateType.Normal, true);
-            s.AppendInterval(0.2f).OnStart(()=> 
-            {
-                //更改映射表由UI到Player
-                foreach (var player in PlayerManager.Instance.players)
-                {
-                    player.GetComponent<PlayerInput>().SwitchCurrentActionMap("UI");
-                }
-                CameraTrace.instance.CameraShake(1f, 2f);
-                MusicManager.Instance.PlaySound("Boss飞石");
-                MusicManager.Instance.PlaySound("潜艇警报");
+            CameraTrace.instance.CameraShake(1f, 2f);
+            MusicManager.Instance.PlaySound("Boss飞石");
+            MusicManager.Instance.PlaySound("潜艇警报");
 
-            }).OnComplete(() =>
-            {
-                Instantiate(bossPrefab, bossInsTran.position, Quaternion.identity);
-                MusicManager.Instance.PlayBackMusic("Boss");
-                GetComponent<SphereCollider>().enabled = false;
-                Time.timeScale = 1;
-                //更改映射表由UI到Player
-                foreach (var player in PlayerManager.Instance.players)
-                {
-                    player.GetComponent<PlayerInput>().SwitchCurrentActionMap("Player");
-                }
-            });
+
+
+            Instantiate(bossPrefab, bossInsTran.position, Quaternion.identity);
+            MusicManager.Instance.PlayBackMusic("Boss");
+            GetComponent<SphereCollider>().enabled = false;
+
+            //Time.timeScale = 0;
+            //Sequence s = DOTween.Sequence();
+            //s.SetUpdate(UpdateType.Normal, true);
+            //s.AppendInterval(0.2f).OnStart(()=> 
+            //{
+            //    //更改映射表由UI到Player
+            //    foreach (var player in PlayerManager.Instance.players)
+            //    {
+            //        player.GetComponent<PlayerInput>().SwitchCurrentActionMap("UI");
+            //    }
+            //    CameraTrace.instance.CameraShake(1f, 2f);
+            //    MusicManager.Instance.PlaySound("Boss飞石");
+            //    MusicManager.Instance.PlaySound("潜艇警报");
+
+            //}).OnComplete(() =>
+            //{
+            //    Instantiate(bossPrefab, bossInsTran.position, Quaternion.identity);
+            //    MusicManager.Instance.PlayBackMusic("Boss");
+            //    GetComponent<SphereCollider>().enabled = false;
+            //    Time.timeScale = 1;
+            //    //更改映射表由UI到Player
+            //    foreach (var player in PlayerManager.Instance.players)
+            //    {
+            //        player.GetComponent<PlayerInput>().SwitchCurrentActionMap("Player");
+            //    }
+            //});
 
             //Instantiate(bossPrefab, bossInsTran.position, Quaternion.identity);
             //MusicManager.Instance.PlayBackMusic("Boss");
